1a9669cbefbd99140e0f4dc6938ef30edde3a10f
Harsh Thalwal <harshthalwal@gmail.com>
2026-04-26 23:22:01 +0530

    kernel: update entry point and linker script

--- kernel/entry.asm
+++ kernel/entry.asm
@@ -1,10 +1,23 @@
 [bits 64]
 [global _start]
 [extern kernel_main]
+[extern bss_start]
+[extern kernel_end]
 
 section .text
 _start:
-    mov rsp, 0x9FC00
+    mov rsp, 0x200000
+
+    ; zero BSS before any C code runs
+    mov rdi, bss_start
+    mov rcx, kernel_end
+    sub rcx, rdi
+    test rcx, rcx
+    jz .bss_done
+    shr rcx, 3
+    xor rax, rax
+    rep stosq
+.bss_done:
     call kernel_main
 .hang:
     hlt
